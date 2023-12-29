942d16ede322a9966019fcb8f363dd9d489d9cc3
JKrecisz <jakub.krecisz@icloud.com>
2023-12-29 05:11:18 +0100

    Refactored backward prop with initalizing weights and biases

--- src/main.py
+++ src/main.py
@@ -1,27 +1,24 @@
 from neural_network import Layer, NeuralNetwork
 from src.data_loader import MNISTDataLoader
-from activations import linear_fun, tanh_fun, softmax_fun
+from activations import linear_fun, softmax_fun, relu_fun
+
 
 def main():
     custom_loader = MNISTDataLoader(train_path="../data/train", test_path="../data/test")
     custom_loader.load_mnist_data(use_local=True)
-    custom_loader.normalize_datasets()
+    custom_loader.normalize_datasets(mean=0.0, std=255.)
 
     layers = [
         Layer(num_neurons=784, function=linear_fun),
-        Layer(num_neurons=128, function=tanh_fun),
+        Layer(num_neurons=10, function=relu_fun),
         Layer(num_neurons=10, function=softmax_fun)
     ]
 
     neural_network = NeuralNetwork(layers=layers, loader=custom_loader)
 
-    epochs = 2
-    learning_rate = 0.001
-    neural_network.train(epochs=epochs, batch_size=16, learning_rate=learning_rate)
-
-    # # Evaluate the performance on the test dataset
-    # accuracy = neural_network.evaluate()
-    # print(f"Accuracy on the test dataset: {accuracy * 100:.2f}%")
+    epochs = 500
+    learning_rate = 0.1
+    neural_network.train(epochs=epochs, batch_size=60000, learning_rate=learning_rate)
 
 
 if __name__ == '__main__':
--- src/neural_network.py
+++ src/neural_network.py
@@ -11,6 +11,9 @@ class Layer:
         self.activation_function = function
         self.weights = None
         self.biases = None
+        self.z = None
+        self.delta_weights = None
+        self.delta_biases = None
 
 class NeuralNetwork(object):
     def __init__(self, layers: List[Layer], loader: MNISTDataLoader):
@@ -24,8 +27,8 @@ class NeuralNetwork(object):
         for i in range(1, self.num_of_layers):
             input_size = self.sizes[i - 1]
             output_size = self.sizes[i]
-            self.layers[i].weights = np.random.randn(output_size, input_size)
-            self.layers[i].biases = np.zeros((output_size, 1))
+            self.layers[i].weights = np.random.rand(output_size, input_size) - 0.5
+            self.layers[i].biases = np.random.rand(output_size, 1) - 0.5
 
     def forward_propagation(self, inputs: np.ndarray) -> np.ndarray:
         layer_outputs = [inputs.reshape((inputs.shape[0], -1)).T]
@@ -34,18 +37,18 @@ class NeuralNetwork(object):
             weights = self.layers[i].weights
             biases = self.layers[i].biases
             z = np.dot(weights, layer_outputs[-1]) + biases
-            self.layers[i].z = z  # Store the value of z
             a = self.layers[i].activation_function.function(z)
+
             layer_outputs.append(a)
+            self.layers[i].z = z
+        self.layers[0].z = layer_outputs[0]
         return layer_outputs[-1]
 
     def train(self, epochs: int, batch_size: int, learning_rate: float):
+        global output, batch_labels
         total_samples = len(self.data_loader.train_dataset.inputs)
 
         for epoch in range(epochs):
-            epoch_loss = 0  # Inicjalizacja sumy straty w danym epochu
-
-            # Permute the data (shuffle) for each epoch
             indices = np.random.permutation(total_samples)
             inputs_shuffled = self.data_loader.train_dataset.inputs[indices]
             labels_shuffled = self.data_loader.train_dataset.labels[indices]
@@ -53,43 +56,43 @@ class NeuralNetwork(object):
             for i in range(0, total_samples, batch_size):
                 batch_inputs = inputs_shuffled[i:i + batch_size]
                 batch_labels = labels_shuffled[i:i + batch_size]
-                one_hot_batch_labels = self.one_hot(batch_labels)
 
                 output = self.forward_propagation(batch_inputs)
-
-                # Compute the loss (you need to implement this method)
-                loss = self.mean_sq_error(output, one_hot_batch_labels)
-                epoch_loss += loss
-
-                if i % batch_size * 500 == 0:
-                    average_loss = epoch_loss / (i / batch_size + 1)
-                    print(f"Epoch {epoch + 1}/{epochs}, Iteration {i} / {total_samples // 16}, Average Loss: {average_loss}")
-
-                # Backpropagation
-                self.backward_propagation(output, one_hot_batch_labels)
-                # Update weights and biases using gradient descent
+                self.backward_propagation(output, batch_labels)
                 self.update_weights_and_biases(learning_rate)
 
-                # Print average loss for the current epoch
-            average_loss = epoch_loss / (total_samples / batch_size)
-            print(f"Epoch {epoch + 1}/{epochs}, Average Loss: {average_loss}")
+            if epoch % 50 == 0:
+                print("Iteration: ", epoch)
+                predictions = self.get_predictions(output)
+                print("Accuracy:", self.get_accuracy(predictions, batch_labels))
 
-    def backward_propagation(self, output, one_hot_batch_labels):
-        pass
+    def backward_propagation(self, output_, batch_labels):
+        total_samples = len(self.data_loader.train_dataset.inputs)
+        delta_output = output_ - self.one_hot(batch_labels)
+
+        for i in range(self.num_of_layers - 1, 0, -1):
+            a_prev = self.layers[i - 1].activation_function.function(self.layers[i - 1].z)
+
+            delta_weights = 1 / total_samples * np.dot(delta_output, a_prev.T)
+            delta_biases = 1 / total_samples * np.sum(delta_output)     # axis=1, keepdims=True
+
+            delta_output = (np.dot(self.layers[i].weights.T, delta_output) *
+                            self.layers[i - 1].activation_function.derivative(self.layers[i - 1].z))
+
+            self.layers[i].delta_weights = delta_weights
+            self.layers[i].delta_biases = delta_biases
 
     def update_weights_and_biases(self, learning_rate: float):
-        # Update weights and biases using the stored gradients
         for i in range(1, self.num_of_layers):
             self.layers[i].weights -= learning_rate * self.layers[i].delta_weights
             self.layers[i].biases -= learning_rate * self.layers[i].delta_biases
 
-            # Clear stored gradients for the next iteration
             self.layers[i].delta_weights = None
             self.layers[i].delta_biases = None
 
     @staticmethod
-    def mean_sq_error(output: np.ndarray, labels: np.ndarray) -> float:
-        return np.mean((output - labels) ** 2)
+    def mean_sq_error(output_: np.ndarray, labels: np.ndarray) -> float:
+        return np.mean((output_ - labels) ** 2)
 
     @staticmethod
     def one_hot(labels: np.ndarray) -> np.ndarray:
@@ -99,6 +102,13 @@ class NeuralNetwork(object):
         :param labels: Integer labels.
         :return: One-hot encoded labels.
         """
-        num_classes = labels.max() + 1
-        one_hot_labels = np.eye(num_classes)[labels].T
+        one_hot_labels = np.eye(10)[labels].T
         return one_hot_labels
+
+    @staticmethod
+    def get_predictions(output):
+        return np.argmax(output, 0)
+
+    @staticmethod
+    def get_accuracy(predictions, batch_labels):
+        return np.sum(predictions == batch_labels) / batch_labels.size
