ee851a836263f4383bc3375d4890dbb328a39bb7
JKrecisz <jakub.krecisz@icloud.com>
2024-02-04 00:39:32 +0100

    Clean up main

--- src/main.py
+++ src/main.py
@@ -1,31 +1,38 @@
-from neural_network import Layer, MNISTNeuralNetwork
+from src.neural_network import Layer, MNISTNeuralNetwork
 from src.data_loader import MNISTDataLoader
-from activations import linear, softmax, relu, tanh
+from src.gui import MNISTGui
+from activations import linear, softmax, relu, tanh, sigmoid
 
 
 def main():
     custom_loader = MNISTDataLoader(train_fpath="../data/train", test_fpath="../data/test")
-    custom_loader.load_mnist_data(use_local=True)
+    custom_loader.load_mnist_data()
     custom_loader.normalize_datasets(mean=0.0, std=255.)
 
-    layers = [
-        Layer(num_neurons=784, function=linear),
-        Layer(num_neurons=400, function=relu),
-        Layer(num_neurons=150, function=tanh),
-        Layer(num_neurons=10, function=softmax)
-    ]
+    model = MNISTNeuralNetwork(
+        layers=[
+            Layer(num_neurons=784, function=linear),
+            Layer(num_neurons=200, function=relu),
+            Layer(num_neurons=150, function=tanh),
+            Layer(num_neurons=10, function=softmax)
+        ],
+        loader=custom_loader
+    )
 
-    neural_network = MNISTNeuralNetwork(layers=layers, loader=custom_loader)
-
-    epochs = 10
+    epochs = 5
     learning_rate = 0.01
-    neural_network.train(epochs=epochs, batch_size=16, decay_rate=0.9, learning_rate=learning_rate)
-    print(f"Test accuracy: {(test_accuracy := neural_network.evaluate())}")
-    MNISTNeuralNetwork.save_model(neural_network, f'../models/trained_model_{test_accuracy * 100:.0f}_acc.csv')
+    model.train(
+        epochs=epochs,
+        batch_size=16,
+        decay_rate=0.7,
+        learning_rate=learning_rate
+    )
 
-    # loaded_model = MNISTNeuralNetwork.load_model('../models/trained_model_89_acc.csv', custom_loader)
-    # test_accuracy_loaded_model = loaded_model.evaluate()
-    # print(f"Test accuracy, tested on 10k samples on loaded model: {test_accuracy_loaded_model}")
+    print(f"Test accuracy of model: {model.evaluate() * 100:.2f}")
+    model.predict(*custom_loader.get_sample(), plot=True)
+
+    # gui = MNISTGui(model)
+    # gui.start()
 
 
 if __name__ == '__main__':
